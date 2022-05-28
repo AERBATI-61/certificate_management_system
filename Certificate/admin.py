6aa2cdb0cd3dada54607633da0f95b488cc3edcc
AERBATI-61 <jasurboy52@gmail.com>
2022-05-28 03:05:34 +0300

    admin finished

--- Certificate/admin.py
+++ Certificate/admin.py
@@ -11,14 +11,28 @@ from django.contrib import messages
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('activity_name', 'starttime', 'endtime')
 
+admin.site.register(Activity, BlogAdmin)
 
 class HostAdmin(admin.ModelAdmin):
 
     def response_change(self, request, obj, post_url_continue=None):
         activity = request.POST.getlist('activity_name')
+
         selected = Activity.objects.filter(id__in=activity)
 
+
         a = len(selected)
+
+        # for t in selected:
+        #     if t.Is_Past == "Past":
+        #         messages.add_message(request, messages.WARNING,
+        #                              f'{t} activity has finished at {t.endtime.date()}')
+        #         return HttpResponseRedirect(request.path_info)
+
+
+
+
+
         today = date.today()
 
         def hata(i, b):
@@ -27,6 +41,7 @@ class HostAdmin(admin.ModelAdmin):
 
         if a > 1 and selected:
             for i in range(a - 1):
+
                 for b in range(i + 1, a):
                     if selected[i].starttime.date() == selected[b].starttime.date():
 
@@ -83,88 +98,30 @@ class HostAdmin(admin.ModelAdmin):
 
 
                         else:
-
                             if selected[i].starttime.date() >= today or selected[b].starttime.date() >= today:
-
-                                messages.add_message(request, messages.WARNING,
-                                                     f'{selected[b]} starts at {selected[b].starttime.date()}.  {selected[i]} starts at {selected[i].starttime.date()}')
-
+                                messages.add_message(request, messages.WARNING, f'{selected[b]} starts at {selected[b].starttime.date()}.  {selected[i]} starts at {selected[i].starttime.date()}')
                                 return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
 
-
                             elif selected[i].starttime.date() <= today or selected[b].starttime.date() <= today:
-
-                                messages.add_message(request, messages.WARNING,
-                                                     f'{selected[b]} finished at {selected[b].starttime.date()}.  {selected[i]} finished at {selected[i].starttime.date()}')
-
-                                return HttpResponseRedirect(request.path_info)
-
-
-
-
-
-                            elif selected[i].starttime.date() <= today:
-
-                                messages.add_message(request, messages.WARNING,
-                                                     f'{selected[i]} finished at {selected[i].starttime.date()}')
-
-                                return HttpResponseRedirect(request.path_info)
-
-
-                            elif selected[b].starttime.date() <= today:
-
-                                messages.add_message(request, messages.WARNING,
-                                                     f'{selected[b]} finished at {selected[b].starttime.date()}')
-
+                                messages.add_message(request, messages.WARNING, f'{selected[b]} finished at {selected[b].starttime.date()}.  {selected[i]} finished at {selected[i].starttime.date()}')
                                 return HttpResponseRedirect(request.path_info)
 
 
                             else:
-
                                 messages.add_message(request, messages.SUCCESS, f'successfully saved')
-
                                 return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
 
-                            # if selected[i].starttime.date() >= selected[b].starttime.date() and selected[b].starttime.date() <= today:
-                            #     messages.add_message(request, messages.WARNING,
-                            #                          f'This activity is end at {selected[b].starttime.date()}')
-                            #     return HttpResponseRedirect(request.path_info)
-                            #
-                            #
-                            # elif selected[b].starttime.date() >= selected[i].starttime.date() and selected[i].starttime.date() <= today:
-                            #     messages.add_message(request, messages.WARNING,
-                            #                          f'This activity is end at {selected[i].starttime.date()}')
-                            #     return HttpResponseRedirect(request.path_info)
 
-                    # elif selected[i].starttime.date() > today:
-                    #     messages.add_message(request, messages.WARNING, f'start time is {selected[i].starttime.date()}')
-                    #     return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
-                    #
-                    # elif selected[i].starttime.date() < today:
-                    #     messages.add_message(request, messages.WARNING, f'finished time is {selected[i].starttime.date()}')
-                    #     return HttpResponseRedirect(request.path_info)
 
-                    elif selected[i].starttime.date() >= selected[b].starttime.date() and selected[
-                        b].starttime.date() <= today:
-                        messages.add_message(request, messages.WARNING,
-                                             f'this {selected[i]} start time is {selected[i].starttime.date()}  this {selected[b]} finished {selected[i].starttime.date()}')
+                    elif selected[i].starttime.date() >= selected[b].starttime.date() and selected[b].starttime.date() <= today:
+                        messages.add_message(request, messages.WARNING, f'this {selected[i]} start time is {selected[i].starttime.date()}  this {selected[b]} finished {selected[b].starttime.date()}')
                         # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
                         return HttpResponseRedirect(request.path_info)
 
-                    elif selected[b].starttime.date() >= selected[i].starttime.date() and selected[
-                        i].starttime.date() <= today:
-                        messages.add_message(request, messages.WARNING,
-                                             f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
+                    elif selected[b].starttime.date() >= selected[i].starttime.date() and selected[i].starttime.date() <= today:
+                        messages.add_message(request, messages.WARNING, f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
                         # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
                         return HttpResponseRedirect(request.path_info)
-
-                    # if selected[i].starttime.date() > today:
-                    #     messages.add_message(request, messages.WARNING, f'start time is {selected[i].starttime.date()}')
-                    # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
-                    # return HttpResponseRedirect(request.path_info)
-
-
-
 
         else:
             messages.add_message(request, messages.SUCCESS, f'successfully saved')
@@ -181,7 +138,12 @@ class ParticipantAdmin(admin.ModelAdmin):
     def response_change(self, request, obj, post_url_continue=None):
         activity = request.POST.getlist('activity_name')
         selected = Activity.objects.filter(id__in=activity)
-        print('selected', selected)
+
+        # for t in selected:
+        #     if t.Is_Past == "Past":
+        #         messages.add_message(request, messages.WARNING,
+        #                              f'{t} activity has finished at {t.endtime.date()}')
+        #         return HttpResponseRedirect(request.path_info)
 
         a = len(selected)
         print(a)
@@ -258,9 +220,6 @@ class ParticipantAdmin(admin.ModelAdmin):
                                                      f'{selected[b]} finished at {selected[b].starttime.date()}.  {selected[i]} finished at {selected[i].starttime.date()}')
                                 return HttpResponseRedirect(request.path_info)
 
-
-
-
                             elif selected[i].starttime.date() <= today:
                                 messages.add_message(request, messages.WARNING,
                                                      f'{selected[i]} finished at {selected[i].starttime.date()}')
@@ -275,39 +234,23 @@ class ParticipantAdmin(admin.ModelAdmin):
                                 messages.add_message(request, messages.SUCCESS, f'successfully saved')
                                 return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/participant/')
 
-                    # elif selected[i].starttime.date() > today:
-                    #     messages.add_message(request, messages.WARNING, f'start time is {selected[i].starttime.date()}')
-                    #     return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/participant/')
-                    # else:
-                    #     if selected[i].starttime.date() >= selected[b].starttime.date():
-                    #         messages.add_message(request, messages.WARNING,
-                    #                              f'This activity is end at {selected[b].starttime.date()}')
-                    #         return HttpResponseRedirect(request.path_info)
-                    #     else:
-                    #         messages.add_message(request, messages.WARNING,
-                    #                              f'This activity is end at {selected[i].starttime.date()}')
-                    #         return HttpResponseRedirect(request.path_info)
 
-                    elif selected[i].starttime.date() >= selected[b].starttime.date() and selected[
-                        b].starttime.date() <= today:
+
+                    elif selected[i].starttime.date() >= selected[b].starttime.date() and selected[b].starttime.date() <= today:
                         messages.add_message(request, messages.WARNING,
                                              f'this {selected[i]} start time is {selected[i].starttime.date()}  this {selected[b]} finished {selected[i].starttime.date()}')
                         # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
                         return HttpResponseRedirect(request.path_info)
 
-                    elif selected[b].starttime.date() >= selected[i].starttime.date() and selected[
-                        i].starttime.date() <= today:
-                        messages.add_message(request, messages.WARNING,
-                                             f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
+                    elif selected[b].starttime.date() >= selected[i].starttime.date() and selected[i].starttime.date() <= today:
+                        messages.add_message(request, messages.WARNING,f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
                         # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
                         return HttpResponseRedirect(request.path_info)
 
                     elif selected[b].starttime.date() >= today or selected[i].starttime.date() >= today:
-                        messages.add_message(request, messages.WARNING,
-                                             f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
+                        messages.add_message(request, messages.WARNING,f'this {selected[b]} start time is {selected[b].starttime.date()}.  this {selected[i]} finished {selected[i].starttime.date()}')
                         # return HttpResponseRedirect('http://127.0.0.1:8000/admin/Certificate/host/')
                         return HttpResponseRedirect(request.path_info)
-
 
         else:
             messages.add_message(request, messages.SUCCESS, f'successfully saved')
@@ -324,6 +267,6 @@ class OrgAdmin(admin.ModelAdmin):
 
 admin.site.register(Organization, OrgAdmin)
 
-admin.site.register(Activity, BlogAdmin)
+
 admin.site.register(Certificate)
 admin.site.register(ContactUs)
