bb602e07f162fe0838ae0c2dd8fcfab935e741d7
AERBATI-61 <jasurboy52@gmail.com>
2022-05-29 16:34:35 +0300

    static

--- certificate_management_system/urls.py
+++ certificate_management_system/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('', include('Certificate.urls')),
     path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
