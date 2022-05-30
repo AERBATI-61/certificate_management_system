21e9cf8f0c45d4c16762c7515a775de78ca8a1a9
AERBATI-61 <jasurboy52@gmail.com>
2022-05-30 05:36:13 +0300

    staticfiels

--- certificate_management_system/settings.py
+++ certificate_management_system/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'Certificate/static'),
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'Certificate/static'
+]
+
+
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'Certificate/static'),
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
