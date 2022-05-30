d094cbce3ad1e26696d70957342d999499509e2c
AERBATI-61 <jasurboy52@gmail.com>
2022-05-30 05:11:07 +0300

    staticfiels

--- certificate_management_system/settings.py
+++ certificate_management_system/settings.py
@@ -120,14 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'Certificate/static'),
-
-
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '../Certificate/static/'),
-# ]
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
